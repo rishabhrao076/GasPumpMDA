269a3bc0bb301d6b33453683eee5b91cfb1542bd
Rishabh Rao <rishabhrao076@gmail.com>
2024-04-24 23:18:19 -0500

    updated comments in DataStores

--- src/edu/iit/project/datastore/DS1.java
+++ src/edu/iit/project/datastore/DS1.java
@@ -1,12 +1,19 @@
 package edu.iit.project.datastore;
 
-public class DS1 extends DataStore{
+public class DS1 extends DataStore {
+    // Temporary Price of Gas
     public int temp_a;
+    // Price of gas
     public int price;
+    //    Amount of litres pumped
     public int L;
+    //    total billing
     public int total;
+    //    temporary storage for cash
     public int temp_c;
+    //    payment type
     public int w;
+    //    amount of cash input in the machine
     public int cash;
 
 }
--- src/edu/iit/project/datastore/DS2.java
+++ src/edu/iit/project/datastore/DS2.java
@@ -1,16 +1,33 @@
 package edu.iit.project.datastore;
 
-public class DS2 extends DataStore{
+public class DS2 extends DataStore {
+    // temp storage of Regular gas price
+
     public float temp_a;
+    // temp storage of Premium gas price
+
     public float temp_b;
+    // temp storage of Diesel price
     public float temp_c;
+    // Temporary storage for cash
     public int temp_cash;
+    // Price of Diesel
     public float Dprice;
+    // Price of Regular
+
     public float Rprice;
+    // Price of Premium
+
     public float Pprice;
+    // Cash Input
 
     public int cash;
+    // Total Billing
+
     public float total;
+
+    //    Gallons of fuel used
     public int G;
+    // Price of price of currently selected gas.
     public float price;
 }
